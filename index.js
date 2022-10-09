a897e98becb19628db6797fb99e2107441947a43
ramses2099 <ramses2099@gmail.com>
2022-10-09 11:16:01 -0400

    draw shape with pixijs and matterjs

--- index.js
+++ index.js
@@ -1,175 +1,78 @@
-const canvas = document.getElementById('divcanva');
+/*-----------------------------------------------------------------------------------
+PIXI JS
+-------------------------------------------------------------------------------------*/
 
-/*--------------------------------------------------------
-Matter js
-----------------------------------------------------------*/
+const app = new PIXI.Application({ width: 800, height: 600 });
 
-const { Engine, Render, World, Runner, Body, Bodies, Composite, Mouse, MouseConstraint, Events} = Matter;
+document.body.appendChild(app.view);
 
-//engine
-const engine = Engine.create();
-// create world
-const world = engine.world;
+/*-----------------------------------------------------------------------------------
+MATTER JS
+-------------------------------------------------------------------------------------*/
 
+const { Body, Engine, Bodies, Collision, World, Runner } = Matter;
 
-//render
-const render = Render.create({
-    element: canvas,
-    engine: engine,
-    options:{
-        width: 800,
-        height: 600
-    }
-});
+//create engine matter js
+let engine = Engine.create();
 
+//create world
+let world = engine.world;
 
-const sprite = {
-    texture:'images/ship_0009.png',
-    xScale: 2,
-    yScale: 2,
-    xOffset: 0,
-    yOffset: 0
-}
+//create runner
+let runner = Runner.create()
+Runner.run(runner, engine);
 
-
-//
-render.options.wireframes = false;
-
-
-/*--------------------------------------------------------
-Mouse
-----------------------------------------------------------*/
-const mouse = Mouse.create(render.canvas);
-const mouseContraint = MouseConstraint.create(engine,{
-    mouse:mouse,
-    constraint:{
-        render: {visible:false}
-    }
-});
-render.mouse = mouse;
-
-
-/*--------------------------------------------------------
-Keyboard
-----------------------------------------------------------*/
-const keyHandlers = {
-    ArrowUp:() => {
-        Body.setVelocity(player.body,{
-            x: player.body.velocity.x,
-            y: (player.yVel + player.speed) * -1
-        })
-    },
-    ArrowDown:() => {
-        Body.setVelocity(player.body,{
-            x: player.body.velocity.x,
-            y: (player.yVel + player.speed) * -1
-        })
-    },
-    ArrowRight:() => {
-        Body.setVelocity(player.body,{
-            x: (player.yVel + player.speed),
-            y:  player.body.velocity.y
-        })
-    },
-    ArrowLeft:() => {
-        Body.setVelocity(player.body,{
-            x: (player.yVel + player.speed) * -1,
-            y:  player.body.velocity.y
-        })
-    },
-};
-
-const keysDown = new Set();
-document.addEventListener("keydown", function(event){
-    keysDown.add(event.code);
-});
-
-document.addEventListener("keyup", function(event){
-    keysDown.delete(event.code);
-});
-
-Events.on(engine, "beforeUpdate", function(event){
-    //keyboard event
-    [...keysDown].forEach(k=>{
-        keyHandlers[k]?.();
-    });
-
-
-});
-
-/*--------------------------------------------------------
-Bodies
-----------------------------------------------------------*/
-
-
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-
-var bodyPlayer = Bodies.rectangle(350, 50, 32, 32 ,{
-    inertia: Infinity,
-    friction: 0.1,
-    ignoreGravity: true,
-    render:{
-        sprite: sprite
-    }
-});
-
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true, render: { fillStyle: '#6F2020' }});
-var left = Bodies.rectangle(0, 0, 10, 2000, { isStatic: true, render: { fillStyle: '#6F2020' }});
-var right = Bodies.rectangle(800, 0, 10, 2000, { isStatic: true, render: { fillStyle: '#6F2020' }});
-
-//Array
-let bodies = new Array();
-
-
-
-
-/*--------------------------------------------------------
-Class
-----------------------------------------------------------*/
 
 class MatterBody{
     constructor(body){
         this.body = body;
         this.dead = false;
-        this.xVel = 0;
-        this.yVel = 0;
-        this.speed = 5;
-
-        World.add(world, this.body);
+        this.graphics = new PIXI.Graphics();        
+        this.lineColor ='0xffffff';
+        this.fillColor =  "0x" + "000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
         
+        //add to pixi js
+        app.stage.addChild(this.graphics);
+       
+        //add matter world
+        World.add(world, this.body);
     }
+
+    draw(){
+        this.graphics.clear();
+        // draw a shape
+        this.graphics.beginFill(this.fillColor);
+        this.graphics.lineStyle(1, this.lineColor, 1);
+
+        this.graphics.moveTo(this.body.vertices[0].x, this.body.vertices[0].y);
+        for (let i = 0; i < this.body.vertices.length; i+=1) {
+            this.graphics.lineTo(this.body.vertices[i].x, this.body.vertices[i].y);            
+        }
+        this.graphics.lineTo(this.body.vertices[0].x, this.body.vertices[0].y);
+
+        this.graphics.closePath();
+        this.graphics.endFill();
+
+    }
+    
 }
 
-const player = new MatterBody(bodyPlayer);
+let bodies = [];
 
-bodies.push(player);
+bodies.push(new MatterBody(new Bodies.rectangle(400, 0, 800, 50,{ isStatic:true })));
+bodies.push(new MatterBody(new Bodies.rectangle(400, 600, 800, 50,{ isStatic:true })));
+bodies.push(new MatterBody(new Bodies.rectangle(800, 300, 50, 600,{ isStatic:true })));
+bodies.push(new MatterBody(new Bodies.rectangle(0, 300, 50, 600,{ isStatic:true })));
 
-bodies.push(...[boxA, boxB, ground, left, right, mouseContraint]);
-
-// add all of the bodies to the world
-Composite.add(world, bodies);
+bodies.push(new MatterBody(new Bodies.rectangle(50, 50, 50, 50,{ isStatic:false })));
 
 
 
-// run the renderer
-Render.run(render);
+//loop
+app.ticker.add((delta) => {
+    
+    bodies.forEach(body => {
+        body.draw();
+    });
 
-// create runner
-var runner = Runner.create({
-    delta: 1000 / 60,
-    isFixed: false,
-    enabled: true
 });
-
-//update
-Events.on(runner,'tick',function(event){
-    let {delta} = event.source;
-    //console.log(delta);
-});
-
-
-
-// run the engine
-Runner.run(runner, engine);
